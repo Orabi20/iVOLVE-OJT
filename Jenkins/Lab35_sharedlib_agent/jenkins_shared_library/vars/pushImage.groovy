32ba147d57ffd77e05f0205a318a9b2fc43aa4df
Ahmed Orabi <ahmedorabi11011@gmail.com>
2025-07-16 15:52:37 +0300

    Update pushImage.groovy

--- Jenkins/Lab35_sharedlib_agent/jenkins_shared_library/vars/pushImage.groovy
+++ Jenkins/Lab35_sharedlib_agent/jenkins_shared_library/vars/pushImage.groovy
@@ -2,7 +2,7 @@ def call(String imageName, String dockerUser, String dockerPass) {
 
     sh """
         echo "${dockerPass}" | docker login -u "${dockerUser}" --password-stdin
-        // docker push ${imageName}
+       
         docker logout
     """
 }
