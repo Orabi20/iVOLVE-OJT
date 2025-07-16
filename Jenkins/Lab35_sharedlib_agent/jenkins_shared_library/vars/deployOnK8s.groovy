7218ec0af07b33714a86b047e4b123935f5de411
Ahmed Orabi <ahmedorabi11011@gmail.com>
2025-07-16 14:14:36 +0300

    Update deployOnK8s.groovy

--- Jenkins/Lab35_sharedlib_agent/jenkins_shared_library/vars/deployOnK8s.groovy
+++ Jenkins/Lab35_sharedlib_agent/jenkins_shared_library/vars/deployOnK8s.groovy
@@ -1,17 +1,29 @@
 def call(String apiServer, String token, String namespace, String imageName, String deploymentFilePath) {
-    def fileName = deploymentFilePath.tokenize("/").last()
-    def dirPath = deploymentFilePath.replace("/${fileName}", "")
+    // Extract file name and parent dir
+    def file = new File(deploymentFilePath)
+    def fileName = file.getName()
+    def dirPath = file.getParent()
 
-    dir(dirPath) {
-        sh """
-            sed -i 's|image:.*|image: ${imageName}|' ${fileName}
-            cat ${fileName}
-        """
-        sh """
-            kubectl --server=${apiServer} \
-                    --token=${token} \
-                    --insecure-skip-tls-verify \
-                    apply -f ${fileName} -n ${namespace} --validate=false
-        """
+    if (dirPath) {
+        // file is inside a subdirectory
+        dir(dirPath) {
+            runDeployCommands(fileName, imageName, apiServer, token, namespace)
+        }
+    } else {
+        // file is in root workspace, don't use dir()
+        runDeployCommands(fileName, imageName, apiServer, token, namespace)
     }
+}
+
+def runDeployCommands(fileName, imageName, apiServer, token, namespace) {
+    sh """
+        sed -i 's|image:.*|image: ${imageName}|' ${fileName}
+        cat ${fileName}
+    """
+    sh """
+        kubectl --server=${apiServer} \
+                --token=${token} \
+                --insecure-skip-tls-verify \
+                apply -f ${fileName} -n ${namespace} --validate=false
+    """
 }
